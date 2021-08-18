315d0497c2daa4e6e1d6273d2719285cd2398ab9
Nimisha-Kani <nimisha.kani@mfine.co>
2021-08-18 21:55:07 +0530

    second commit

--- app.js
+++ app.js
@@ -2,44 +2,94 @@ let num = document.getElementById("number");
 let consol = document.getElementById("console");
 let numberList = document.getElementById("numberList");
 let items = numberList.getElementsByTagName("li");
-var output = '';
-function addlist(){
-    if(num.value !=''){
-        
-        output += `<li> ${num.value} </li>`;
-    }
-    num.value.innerHTML = '';
-    document.getElementById("numberList").innerHTML = output;
-    
-}
+// var output = '';
+
 function createMenuItem(name) {
     let li = document.createElement('li');
     li.textContent = name;
     return li;
 }
+function addlist(){
+    if(num.value !=''){
+        
+        // output += `<li> ${num.value} </li>`;
+        numberList.appendChild(createMenuItem(num.value));
+        num.value.innerHTML = '';
+    }
+    
+    // document.getElementById("numberList").innerHTML = output;
+    
+}
+
 
 function sum(){
+    consol.style.display = "block";
     var res= 0;
-    for(i=0;i<items.length;i++){
-        
-        res += parseInt(items[i]);
-        console.log(items[i]);
+    var tab = [];
+    
+        // add values to the array
+    for(var i = 0; i < items.length; i++){
+       tab.push(items[i].innerHTML);
     }
-    consol.innerHTML = `Sum is ${res}`;
+    console.log('tab array ',tab);
+    for(i=0;i<tab.length;i++){
+        
+        res += parseInt(tab[i]);
+        console.log(tab[i]);
+    }
+   
+    consol.innerHTML += `<br> Sum is ${res}`;
+    
 }
 
 function average(){
     var res= 0;
-    for(i=0;i<items.length;i++){
+    var tab = [];
         
-        res += parseInt(items[i]);
-        console.log(items[i]);
+        // add values to the array
+    for(var i = 0; i < items.length; i++){
+       tab.push(items[i].innerHTML);
     }
-    var avg = res/items.length;
-    consol.innerHTML = `Average is ${avg}`;
+    for(i=0;i<tab.length;i++){
+        
+        res += parseInt(tab[i]);
+        
+    }
+    var avg = res/tab.length;
+    
+    consol.innerHTML += `<br> Average is ${avg}`;
+    consol.style.display = "block";
 }
 
-function clear(){
-    consol.style.display = "none";
+function findMin(){
+    var res= 0;
+    var tab = [];
+        
+        // add values to the array
+    for(var i = 0; i < items.length; i++){
+        tab.push(parseInt(items[i].innerHTML));
+
+    }
+    // console.log("inside find min",tab);
+    var minimum = Math.min(...tab);
+    consol.innerHTML += `<br> Smallest element is ${minimum}`;
+    consol.style.display = "block";
+}
+
+function clearDisplay(){
+    // consol.textContent = " ";
+    console.log("inside clear method");
+    consol.style.display = 'none';
+    numberList.style.display ="none";
     
 }
+
+function reset(){
+    consol.innerHTML = "";
+    numberList.innerHTML = "";
+}
+
+function refresh(){
+    numberList.style.display = "block";
+}
+
